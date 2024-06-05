63d9b970943a07bf835afef5fb67ebc873b21ea7
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-05 19:12:30 -0300

    Add implementation BaseRepository

---
+++ src/AthenasAcademy.Certificate.Core/Repositories/Postgre/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using AthenasAcademy.Certificate.Core.Configurations.Settings;
+using Npgsql;
+
+namespace AthenasAcademy.Certificate.Core;
+
+public class BaseRepository
+{
+    private readonly PostgreSettings _settings;
+    public BaseRepository(PostgreSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public async Task<IDbConnection> GetConnectionAsync()
+    {
+        NpgsqlConnection connection = new(GetConnectionString());
+        await connection.OpenAsync();
+        return connection;
+    }
+
+    public IDbConnection GetConnection()
+    {
+        NpgsqlConnection connection = new(GetConnectionString());
+        connection.Open();
+        return connection;
+    }
+
+    private string GetConnectionString()
+    {
+        NpgsqlConnectionStringBuilder connectionString = new()
+        {
+            Host = _settings.Host,
+            Port = _settings.Port,
+            Database = _settings.Database,
+            Username = _settings.Username,
+            Password = _settings.Password,
+            Timeout = 20,
+            CommandTimeout = 20,
+            SslMode = _settings.SslMode ? 
+                        SslMode.Disable : SslMode.Allow,
+        };
+        return connectionString.ConnectionString;
+    }
+}
