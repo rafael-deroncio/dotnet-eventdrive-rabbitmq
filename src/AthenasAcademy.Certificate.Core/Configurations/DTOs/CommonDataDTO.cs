ff535d0394ddf6a6aedbad331f720d82d79f6d4e
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-06 21:39:32 -0300

    Add dtos

---
+++ src/AthenasAcademy.Certificate.Core/Configurations/DTOs/CommonDataDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AthenasAcademy.Certificate.Core.Configurations.DTOs;
+
+public record CommonDataDTO
+{
+    public int Id { get; set; }
+    public bool Active { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+}
