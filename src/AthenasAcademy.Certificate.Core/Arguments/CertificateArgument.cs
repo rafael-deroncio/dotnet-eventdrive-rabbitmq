0ad6278de65a35a2cd3d0f9f6ab1c861dba3207e
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-07 14:38:04 -0300

    Add Arguments

---
+++ src/AthenasAcademy.Certificate.Core/Arguments/CertificateArgument.cs
@@ -0,0 +1,13 @@
+﻿namespace AthenasAcademy.Certificate.Core.Arguments;
+
+public record CertificateArgument
+{
+    public int Id { get; set; }
+    public string Student { get; set; }
+    public string Document { get; set; }
+    public string Registration { get; set; }
+    public string Course { get; set; }
+    public DateTime Completion { get; set; }
+    public decimal Utilization { get; set; }
+    public List<FileDetailArgument> FileDetails { get; set; }
+}
---
+++ src/AthenasAcademy.Certificate.Core/Arguments/FileDetailArgument.cs
@@ -0,0 +1,10 @@
+﻿namespace AthenasAcademy.Certificate.Core.Arguments;
+
+public record class FileDetailArgument
+{
+    public int Id { get; set; }
+    public string File { get; set; }
+    public string Type { get; set; }
+    public string Path { get; set; }
+    public int Size { get; set; }
+}
