c9dce88e0760eecdec036a97fd84f808283dfe03
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-07 20:04:02 -0300

    refactor sql

--- deploy/certificate-create.sql
+++ deploy/certificate-create.sql
@@ -6,48 +6,26 @@ DROP TABLE IF EXISTS event_proccess;
 DROP TABLE IF EXISTS event_proccess_status;
 
 -- Drop existing sequences if they exist
-DROP SEQUENCE IF EXISTS file_details_id_seq;
-DROP SEQUENCE IF EXISTS certificate_files_id_seq;
-DROP SEQUENCE IF EXISTS certificates_id_seq;
-DROP SEQUENCE IF EXISTS event_proccess_id_seq;
+DROP SEQUENCE IF EXISTS file_details_cod_seq;
+DROP SEQUENCE IF EXISTS certificate_files_cod_seq;
+DROP SEQUENCE IF EXISTS certificates_cod_seq;
+DROP SEQUENCE IF EXISTS event_proccess_cod_seq;
 
 -- Create sequences
-CREATE SEQUENCE file_details_id_seq;
-CREATE SEQUENCE certificate_files_id_seq;
-CREATE SEQUENCE certificates_id_seq;
-CREATE SEQUENCE event_proccess_id_seq;
+CREATE SEQUENCE file_details_cod_seq;
+CREATE SEQUENCE certificate_files_cod_seq;
+CREATE SEQUENCE certificates_cod_seq;
+CREATE SEQUENCE event_proccess_cod_seq;
 
 -- Set the starting value of the sequences to a random number
-SELECT setval('file_details_id_seq', round(random() * (99999 - 10000) + 10000)::bigint);
-SELECT setval('certificate_files_id_seq', round(random() * (99999 - 10000) + 10000)::bigint);
-SELECT setval('certificates_id_seq', round(random() * (99999 - 10000) + 10000)::bigint);
-SELECT setval('event_proccess_id_seq', round(random() * (99999 - 10000) + 10000)::bigint);
-
--- Create the file_details table with random sequence
-CREATE TABLE file_details (
-  id INTEGER PRIMARY KEY DEFAULT nextval('file_details_id_seq'),
-  file TEXT NOT NULL,
-  type VARCHAR(50) NOT NULL,
-  path VARCHAR(255) NOT NULL,
-  size INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
--- Create the certificate_files table with random sequence
-CREATE TABLE certificate_files (
-  id INTEGER PRIMARY KEY DEFAULT nextval('certificate_files_id_seq'),
-  id_file_details INTEGER NOT NULL,
-  active BOOLEAN NOT NULL DEFAULT false,
-  created TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated TIMESTAMP NOT NULL DEFAULT NOW(),
-  CONSTRAINT fk_file_details FOREIGN KEY (id_file_details) REFERENCES file_details (id) ON DELETE CASCADE
-);
+SELECT setval('file_details_cod_seq', round(random() * (99999 - 10000) + 10000)::bigint);
+SELECT setval('certificate_files_cod_seq', round(random() * (99999 - 10000) + 10000)::bigint);
+SELECT setval('certificates_cod_seq', round(random() * (99999 - 10000) + 10000)::bigint);
+SELECT setval('event_proccess_cod_seq', round(random() * (99999 - 10000) + 10000)::bigint);
 
 -- Create the certificates table with random sequence
 CREATE TABLE certificates (
-  id INTEGER PRIMARY KEY DEFAULT nextval('certificates_id_seq'),
-  id_certificate_files INTEGER,
+  cod_certificate INTEGER PRIMARY KEY DEFAULT nextval('certificates_id_seq'),
   student VARCHAR(100) NOT NULL,
   document VARCHAR(100) NOT NULL,
   registration VARCHAR(50) NOT NULL,
@@ -57,17 +35,36 @@ CREATE TABLE certificates (
   active BOOLEAN NOT NULL DEFAULT false,
   created TIMESTAMP NOT NULL DEFAULT NOW(),
   updated TIMESTAMP NOT NULL DEFAULT NOW(),
-  CONSTRAINT fk_certificate_files FOREIGN KEY (id_certificate_files) REFERENCES certificate_files (id) ON DELETE CASCADE
+);
+
+-- Create the file_details table with random sequence
+CREATE TABLE file_details (
+  cod_file INTEGER PRIMARY KEY DEFAULT nextval('file_details_id_seq'),
+  file TEXT NOT NULL,
+  type VARCHAR(50) NOT NULL,
+  path VARCHAR(255) NOT NULL,
+  size INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated TIMESTAMP NOT NULL DEFAULT NOW(),
+);
+
+-- Create the certificate_files table with random sequence
+CREATE TABLE certificate_files (
+  cod_certificate_file INTEGER PRIMARY KEY DEFAULT nextval('certificate_files_id_seq'),
+  cod_certificate INTEGER NOT NULL,
+  cod_file_detail INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- Create the event_proccess_status table with random sequence
 CREATE TABLE event_proccess_status (
-  id INTEGER PRIMARY KEY,
-  status VARCHAR(50) NOT NULL,
+  cod_event_status INTEGER PRIMARY KEY,
+  desc_status VARCHAR(50) NOT NULL,
   active BOOLEAN NOT NULL DEFAULT false,
   created TIMESTAMP NOT NULL DEFAULT NOW(),
   updated TIMESTAMP NOT NULL DEFAULT NOW(),
-  CONSTRAINT ck_status CHECK (status IN ('OnProcecess', 'Pending', 'Success', 'Error'))
+  CONSTRAINT ck_status CHECK (desc_status IN ('OnProcecess', 'Pending', 'Success', 'Error'))
 );
 
 -- Insert initial values into event_proccess_status with random IDs
@@ -80,8 +77,8 @@ VALUES
 
 -- Create the event_proccess table with random sequence
 CREATE TABLE event_proccess (
-  id INTEGER PRIMARY KEY DEFAULT nextval('event_proccess_id_seq'),
-  id_status INTEGER NOT NULL,
+  cod_event_proccess INTEGER PRIMARY KEY DEFAULT nextval('event_proccess_id_seq'),
+  cod_event_status INTEGER NOT NULL,
   error TEXT NOT NULL,
   attempts INTEGER NOT NULL DEFAULT 0,
   json_event JSONB NOT NULL,
@@ -89,5 +86,5 @@ CREATE TABLE event_proccess (
   active BOOLEAN NOT NULL DEFAULT false,
   created TIMESTAMP NOT NULL DEFAULT NOW(),
   updated TIMESTAMP NOT NULL DEFAULT NOW(),
-  CONSTRAINT fk_event_status FOREIGN KEY (id_status) REFERENCES event_proccess_status (id) ON DELETE CASCADE
+  CONSTRAINT fk_event_status FOREIGN KEY (cod_event_status) REFERENCES event_proccess_status (cod_event_status) ON DELETE CASCADE
 );
