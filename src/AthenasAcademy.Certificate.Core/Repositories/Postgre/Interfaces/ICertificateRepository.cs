bfd17f9c01591682eaeb85234c1bc390bb8715f8
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-07 14:39:45 -0300

    Add contract of Certificate Respository

---
+++ src/AthenasAcademy.Certificate.Core/Repositories/Postgre/Interfaces/ICertificateRepository.cs
@@ -0,0 +1,10 @@
+﻿using AthenasAcademy.Certificate.Core.Arguments;
+using AthenasAcademy.Certificate.Core.Models;
+
+namespace AthenasAcademy.Certificate.Core.Repositories.Postgres.Interfaces;
+
+public interface ICertificateRepository
+{
+    Task<CertificateModel> SaveCertificate(CertificateArgument argumet);
+    Task<CertificateModel> GetCertificateByRegistration(string registration);
+}
