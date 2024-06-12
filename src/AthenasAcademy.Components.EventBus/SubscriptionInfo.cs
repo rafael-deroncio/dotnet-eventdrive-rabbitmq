593a62898503b1005d913a3a3f41722c94f61fc7
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-11 21:47:36 -0300

    SubscriptionInfo doccumented

--- src/AthenasAcademy.Components.EventBus/SubscriptionInfo.cs
+++ src/AthenasAcademy.Components.EventBus/SubscriptionInfo.cs
@@ -1,16 +1,31 @@
 ﻿namespace AthenasAcademy.Components.EventBus;
 
-public class SubscriptionInfo{
+/// <summary>
+/// Represents information about an event subscription, including the handler type.
+/// </summary>
+public class SubscriptionInfo
+{
+    /// <summary>
+    /// Gets the type of the handler associated with the event subscription.
+    /// </summary>
     public Type HandlerType { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SubscriptionInfo"/> class with the specified handler type.
+    /// </summary>
+    /// <param name="handlerType">The type of the handler associated with the event subscription.</param>
     private SubscriptionInfo(Type handlerType)
     {
         HandlerType = handlerType;
     }
 
+    /// <summary>
+    /// Creates a new instance of <see cref="SubscriptionInfo"/> with the specified handler type.
+    /// </summary>
+    /// <param name="handlerType">The type of the handler to be associated with the event subscription.</param>
+    /// <returns>A new instance of <see cref="SubscriptionInfo"/> with the specified handler type.</returns>
     public static SubscriptionInfo Typed(Type handlerType)
     {
         return new SubscriptionInfo(handlerType);
     }
 }
-
