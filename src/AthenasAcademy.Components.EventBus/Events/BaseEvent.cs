088b5961b1aa8553ffba32f075e61a23d82c5903
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-11 22:01:48 -0300

    event base and event handler contract documented

--- src/AthenasAcademy.Components.EventBus/Events/BaseEvent.cs
+++ src/AthenasAcademy.Components.EventBus/Events/BaseEvent.cs
@@ -2,25 +2,46 @@
 
 namespace AthenasAcademy.Components.EventBus.Events;
 
+/// <summary>
+/// Represents the base class for all events in the EventBus system.
+/// </summary>
 public class BaseEvent : IDisposable
 {
+    /// <summary>
+    /// Gets the unique identifier for the event.
+    /// </summary>
     [JsonIgnore]
     public Guid Id { get; private set; } = Guid.NewGuid();
 
+    /// <summary>
+    /// Gets the creation date of the event.
+    /// </summary>
     [JsonIgnore]
     public DateTime CreationDate { get; private set; } = DateTime.Now;
 
+    /// <summary>
+    /// Gets or sets the retry count for the event.
+    /// </summary>
     [JsonIgnore]
     public int RetryCount { get; set; } = 0;
 
     private bool _disposed;
 
+    /// <summary>
+    /// Releases the unmanaged resources used by the BaseEvent and optionally releases the managed resources.
+    /// </summary>
+    /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
     protected virtual void Dispose(bool disposing)
     {
-        if(!_disposed && disposing) 
+        if (!_disposed && disposing)
+        {
             _disposed = true;
+        }
     }
 
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
     public void Dispose()
     {
         Dispose(disposing: true);
