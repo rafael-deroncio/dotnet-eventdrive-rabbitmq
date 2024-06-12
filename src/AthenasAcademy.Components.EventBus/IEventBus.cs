de124b77b241876ba8eff07c9764469ea22cda6c
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-11 21:57:57 -0300

    IEventBus documented

--- src/AthenasAcademy.Components.EventBus/IEventBus.cs
+++ src/AthenasAcademy.Components.EventBus/IEventBus.cs
@@ -3,25 +3,65 @@ using AthenasAcademy.Components.EventBus.Handlers.Interfaces;
 
 namespace AthenasAcademy.Components.EventBus;
 
+/// <summary>
+/// Interface that defines the contract for the EventBus system, providing methods for publishing and subscribing to events.
+/// </summary>
 public interface IEventBus
 {
-    void Publish(BaseEvent @event);
+    /// <summary>
+    /// Asynchronously publishes an event to the EventBus.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <param name="event">The event to be published.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task PublishAsync<TEvent>(TEvent @event)
+        where TEvent : BaseEvent;
 
-    Task PublishAsync(BaseEvent @event);
-
-    void Subscribe<TEvent, TEventHandler>()
+    /// <summary>
+    /// Asynchronously subscribes to a specific event.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <typeparam name="TEventHandler">The type of the handler that will process the event.</typeparam>
+    /// <param name="cancellation">A cancellation token to observe while waiting for the task to complete.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task SubscribeAsync<TEvent, TEventHandler>(CancellationToken cancellation)
         where TEvent : BaseEvent
         where TEventHandler : IEventHandler<TEvent>;
 
-    Task SubscribeAsync<TEvent, TEventHandler>()
-        where TEvent : BaseEvent
-        where TEventHandler : IEventHandler<TEvent>;
-
-    void Unsubscribe<TEvent, TEventHandler>()
-        where TEvent : BaseEvent
-        where TEventHandler : IEventHandler<TEvent>;
-
+    /// <summary>
+    /// Asynchronously unsubscribes from a specific event.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <typeparam name="TEventHandler">The type of the handler that processes the event.</typeparam>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     Task UnsubscribeAsync<TEvent, TEventHandler>()
+        where TEvent : BaseEvent
+        where TEventHandler : IEventHandler<TEvent>;
+
+    /// <summary>
+    /// Publishes an event to the EventBus.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <param name="event">The event to be published.</param>
+    void Publish<TEvent>(TEvent @event)
+        where TEvent : BaseEvent;
+
+    /// <summary>
+    /// Subscribes to a specific event.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <typeparam name="TEventHandler">The type of the handler that will process the event.</typeparam>
+    /// <param name="cancellation">A cancellation token to observe while waiting for the task to complete.</param>
+    void Subscribe<TEvent, TEventHandler>(CancellationToken cancellation)
+        where TEvent : BaseEvent
+        where TEventHandler : IEventHandler<TEvent>;
+
+    /// <summary>
+    /// Unsubscribes from a specific event.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <typeparam name="TEventHandler">The type of the handler that processes the event.</typeparam>
+    void Unsubscribe<TEvent, TEventHandler>()
         where TEvent : BaseEvent
         where TEventHandler : IEventHandler<TEvent>;
 }
