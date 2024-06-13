1c9bbb5752922fca28852f814c9625bc8785bb21
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-13 13:44:24 -0300

    Add constants from rabbitmq

---
+++ src/AthenasAcademy.Components.EventBus/Brokers/RabbitMQConstants.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+
+namespace AthenasAcademy.Components.EventBus.Brokers.Constants;
+
+public record RabbitMQConstants
+{
+    public const string AUTOFAC_SCOPE_NAME = "event_bus_rabbitmq";
+    public const string SUFIX_EVENT = "Event";
+    public const string SUFIX_EVENT_HANDLER = "EventHandler";
+    public const string EXCHANGE_TYPE = ExchangeType.Direct;
+    public const string PREFIX_EXCHANGE = "exchange";
+    public const string PREFIX_QUEUE = "queue";
+    public const string PREFIX_ROUTING_KEY = "key_event";
+    public const string PREFIX_DLQ_EXCHANGE = "dql_exchange";
+    public const string PREFIX_DLQ_QUEUE = "dql_queue";
+    public const string X_DLQ_EXCHANGE = "x-dead-letter-exchange";
+}
