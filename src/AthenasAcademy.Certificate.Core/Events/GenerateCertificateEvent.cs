987b6a4c9f74d871e34a802d7a910cde2e2b26f5
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-15 16:14:22 -0300

    add event

---
+++ src/AthenasAcademy.Certificate.Core/Events/GenerateCertificateEvent.cs
@@ -0,0 +1,8 @@
+﻿using AthenasAcademy.Components.EventBus;
+
+namespace AthenasAcademy.Certificate.Core.Events;
+
+public class GenerateCertificateEvent : BaseEvent
+{
+    public int CodeEventProccess { get; set; }
+}
