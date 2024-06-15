f0b768676f28982cfcb939d4a2a1266ebab41cbf
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-15 16:14:39 -0300

    add services

--- src/AthenasAcademy.Certificate.Core/Services/CertificateService.cs
+++ src/AthenasAcademy.Certificate.Core/Services/CertificateService.cs
@@ -1,31 +1,38 @@
-﻿using System.Text.Json;
-using AthenasAcademy.Certificate.Core.Configurations.Mapper.Interfaces;
+﻿using AthenasAcademy.Certificate.Core.Configurations.Mapper.Interfaces;
+using AthenasAcademy.Certificate.Core.Events;
 using AthenasAcademy.Certificate.Core.Extensions;
+using AthenasAcademy.Certificate.Core.Repositories.Bucket.Interfaces;
 using AthenasAcademy.Certificate.Core.Repositories.Postgres.Interfaces;
 using AthenasAcademy.Certificate.Core.Services.Interfaces;
 using AthenasAcademy.Certificate.Domain;
 using AthenasAcademy.Certificate.Domain.Requests;
 using AthenasAcademy.Certificate.Domain.Responses;
+using AthenasAcademy.Components.EventBus;
 
 namespace AthenasAcademy.Certificate.Core.Services;
 
 public class CertificateService(
     ILogger<CertificateService> logger,
     IObjectConverter mapper,
-    ICertificateRepository certificateRepository
-
+    IEventBus eventBus,
+    ICertificateRepository certificateRepository,
+    IBucketRepository bucketRepository
 ) : ICertificateService
 {
     private readonly ILogger<CertificateService> _logger = logger;
     private readonly IObjectConverter _mapper = mapper;
+    private readonly IEventBus _eventBus = eventBus;
     private readonly ICertificateRepository _certificateRepository = certificateRepository;
+    private readonly IBucketRepository _bucketRepository = bucketRepository;
 
-    public Task<CertificateResponse> Generate(CertificateRequest request)
+    public async Task<CertificateResponse> Generate(CertificateRequest request)
     {
         _logger.LogInformation("Start proccess request for generate certificate.");
         try
         {
-            throw new NotImplementedException();
+            await _eventBus.PublishAsync(new TesteCertificateEvent() { CodeEventProccess = new Random().Next() });
+            await _eventBus.PublishAsync(new GenerateCertificateEvent() { CodeEventProccess = new Random().Next() });
+            return new();
         }
         catch (Exception exception)
         {
@@ -43,6 +50,8 @@ public class CertificateService(
         _logger.LogInformation("Start proccess request for get certificate {0}.", type.GetName());
         try
         {
+            var filename = $"{type.GetName().ToLower()}/teste.pdf";
+            var arquivo = _bucketRepository.GetFile(filename);
             throw new NotImplementedException();
         }
         catch (Exception exception)
