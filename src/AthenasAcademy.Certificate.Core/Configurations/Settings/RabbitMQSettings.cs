808c19032fc6048400de625b9f24d6771f405164
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-15 16:14:52 -0300

    remove settings

--- src/AthenasAcademy.Certificate.Core/Configurations/Settings/RabbitMQSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AthenasAcademy.Certificate.Core.Configurations.Settings;
-
-public record RabbitMQSettings
-{
-    public string Host { get; set; }
-    public int Port { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
