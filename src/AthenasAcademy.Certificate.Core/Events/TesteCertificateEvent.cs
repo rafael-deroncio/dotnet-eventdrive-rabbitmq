c55335a994f678b46fe0d202ada80d13231525b9
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-15 16:14:13 -0300

    add event teste

---
+++ src/AthenasAcademy.Certificate.Core/Events/TesteCertificateEvent.cs
@@ -0,0 +1,8 @@
+﻿using AthenasAcademy.Components.EventBus;
+
+namespace AthenasAcademy.Certificate.Core.Events;
+
+public class TesteCertificateEvent : BaseEvent
+{
+    public int CodeEventProccess { get; set; }
+}
