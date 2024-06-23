5be1a2d9f4ea2a47d98fd26936eecd88719e9c52
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-23 16:51:12 -0300

    Updated settings AWS and parameters

--- src/AthenasAcademy.Certificate.Core/Configurations/Parameters.cs
+++ src/AthenasAcademy.Certificate.Core/Configurations/Parameters.cs
@@ -2,11 +2,13 @@ namespace AthenasAcademy.Certificate.Core.Configurations;
 
 public record Parameters
 {
+    public string BucketName { get; set; }
     public string PathPdf { get; set; }
     public string PathPng { get; set; }
     public string PathQR { get; set; }
-    public string PathTemplate { get; set; }
     public string KeyStamp { get; set; }
     public string KeyLogo { get; set; }
+    public string KeyTemplate { get; set; }
+    public int Expires { get; set; }
     public int MaxAttempsEvent { get; set; }
 }
--- src/AthenasAcademy.Certificate.Core/Configurations/Settings/AWSSettings.cs
+++ src/AthenasAcademy.Certificate.Core/Configurations/Settings/AWSSettings.cs
@@ -5,8 +5,7 @@ public record AWSSettings
     public string ServiceURL { get; set; }
     public string AccessKey { get; set; }
     public string SecretKey { get; set; }
-    public string BucketName { get; set; }
     public string Region { get; set; }
-    public int Expires { get; set; }
+    public string SignatureVersion { get; set; }
     public bool ForcePathStyle { get; set; }
 }
