07dbc134ede153adecd849b96b52703a1be19baf
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-06-30 16:31:15 -0300

    double to decimal

--- src/AthenasAcademy.Certificate.Domain/Requests/CertificateRequest.cs
+++ src/AthenasAcademy.Certificate.Domain/Requests/CertificateRequest.cs
@@ -4,7 +4,7 @@ public record CertificateRequest
 {
     public StudentRequest Student { get; set; }
     public CourseRequest Course { get; set; }
-    public double Utilization { get; set; }
+    public decimal Utilization { get; set; }
     public DateTime ConclusionDate { get; set; }
 
 }
--- src/AthenasAcademy.Certificate.Domain/Requests/DisciplineRequest.cs
+++ src/AthenasAcademy.Certificate.Domain/Requests/DisciplineRequest.cs
@@ -4,6 +4,6 @@ public record DisciplineRequest
 {
     public string Name { get; set; }
     public int Workload { get; set; }
-    public double Percentage { get; set; }
+    public decimal Utilization { get; set; }
     public DateTime ConclusionDate { get; set; }
 }
