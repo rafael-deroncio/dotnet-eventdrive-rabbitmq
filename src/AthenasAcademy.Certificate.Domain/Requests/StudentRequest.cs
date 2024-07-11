292df6e9b93a22071ab968004bd58212a43aaa27
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-10 23:06:39 -0300

    StudentRequest renamed

--- src/AthenasAcademy.Certificate.Domain/Requests/StudentRequest.cs
+++ src/AthenasAcademy.Certificate.Domain/Requests/StudentRequest.cs
@@ -3,7 +3,6 @@
 public record StudentRequest
 {
     public string Name { get; set; }
-    public DateTime BornDate { get; set; }
     public string Registration { get; set; }
     public DocumentRequest Document { get; set; }
 }
