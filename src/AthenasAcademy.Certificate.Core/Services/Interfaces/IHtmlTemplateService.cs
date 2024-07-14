8efac485a86232714bd311ca4a0c7aef6e8a0ba6
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-14 11:21:26 -0300

    add doc of IHtmlTemplateService

--- src/AthenasAcademy.Certificate.Core/Services/Interfaces/IHtmlTemplateService.cs
+++ src/AthenasAcademy.Certificate.Core/Services/Interfaces/IHtmlTemplateService.cs
@@ -2,7 +2,16 @@
 
 namespace AthenasAcademy.Certificate.Core.Services.Interfaces;
 
+/// <summary>
+/// Interface for HTML Template Service
+/// </summary>
 public interface IHtmlTemplateService
 {
-    Task<string> GetHtml(object obj, string template); 
+    /// <summary>
+    /// Generates HTML content based on the provided object and template.
+    /// </summary>
+    /// <param name="obj">The object containing the data to be used in the template.</param>
+    /// <param name="template">The HTML template string.</param>
+    /// <returns>A task representing the asynchronous operation, with a string result containing the generated HTML content.</returns>
+    Task<string> ParseParametersToTemplate(object obj, string template); 
 }
