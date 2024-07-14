43232aaebf188fc4e489a03e1e12b55645bc7467
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-14 11:21:39 -0300

    add doc of IPDFService

--- src/AthenasAcademy.Certificate.Core/Services/Interfaces/IPDFService.cs
+++ src/AthenasAcademy.Certificate.Core/Services/Interfaces/IPDFService.cs
@@ -1,6 +1,14 @@
 ﻿namespace AthenasAcademy.Certificate.Core.Services.Interfaces;
 
+/// <summary>
+/// Interface for PDF Service
+/// </summary>
 public interface IPDFService
 {
+    /// <summary>
+    /// Converts HTML content to a PDF document.
+    /// </summary>
+    /// <param name="html">The HTML content to be converted.</param>
+    /// <returns>A task representing the asynchronous operation, with a byte array result containing the PDF document.</returns>
     Task<byte[]> ConvertHTMLToPDF(string html);
 }
