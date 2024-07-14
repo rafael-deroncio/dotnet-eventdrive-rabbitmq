483e75e8c73a0a432ee897603bfbd01fd52cb9d8
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-14 11:21:06 -0300

    add doc of ICertificateService

--- src/AthenasAcademy.Certificate.Core/Services/Interfaces/ICertificateService.cs
+++ src/AthenasAcademy.Certificate.Core/Services/Interfaces/ICertificateService.cs
@@ -4,8 +4,22 @@ using AthenasAcademy.Certificate.Domain.Responses;
 
 namespace AthenasAcademy.Certificate.Core.Services.Interfaces;
 
+/// <summary>
+/// Interface for Certificate Service
+/// </summary>
 public interface ICertificateService
 {
+    /// <summary>
+    /// Creates a new certificate based on the provided request.
+    /// </summary>
+    /// <param name="request">The request containing the necessary data to create a certificate.</param>
+    /// <returns>A task representing the asynchronous operation, with a CertificateResponse as the result.</returns>
     Task<CertificateResponse> CreateCertificate(CertificateRequest request);
+
+    /// <summary>
+    /// Retrieves a certificate based on the registration identifier.
+    /// </summary>
+    /// <param name="registration">The registration identifier of the certificate.</param>
+    /// <returns>A task representing the asynchronous operation, with a CertificateResponse as the result.</returns>
     Task<CertificateResponse> GetCertificate(string registration);
 }
