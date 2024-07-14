2fd358441cfbbcacdf533b7269bc639dd76f649a
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-14 11:22:04 -0300

    add doc of IProcessEventService

--- src/AthenasAcademy.Certificate.Core/Services/Interfaces/IProccessEventService.cs
+++ src/AthenasAcademy.Certificate.Core/Services/Interfaces/IProccessEventService.cs
@@ -2,7 +2,16 @@
 
 namespace AthenasAcademy.Certificate.Core.Services.Interfaces;
 
-public interface IProccessEventService
+/// <summary>
+/// Interface for Process Event Service
+/// </summary>
+public interface IProcessEventService
 {
-    Task GenerateCertificate(long proccess, CertificateRequest request);
+    /// <summary>
+    /// Generates a certificate based on the provided process ID and certificate request.
+    /// </summary>
+    /// <param name="process">The process ID associated with the certificate generation.</param>
+    /// <param name="request">The request containing the necessary data to generate a certificate.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task GenerateCertificate(long process, CertificateRequest request);
 }
