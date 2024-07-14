e4dbebe2b243d2bdcd6af22bdc88a2f0bf642353
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-14 11:22:19 -0300

    add doc of IQRCodeService

--- src/AthenasAcademy.Certificate.Core/Services/Interfaces/IQRCodeService.cs
+++ src/AthenasAcademy.Certificate.Core/Services/Interfaces/IQRCodeService.cs
@@ -2,7 +2,15 @@
 
 namespace AthenasAcademy.Certificate.Core.Services.Interfaces;
 
+/// <summary>
+/// Interface for QR Code Service
+/// </summary>
 public interface IQRCodeService
 {
+    /// <summary>
+    /// Generates a QR code from the provided text.
+    /// </summary>
+    /// <param name="text">The text to be encoded in the QR code.</param>
+    /// <returns>A task representing the asynchronous operation, with a byte array result containing the QR code image data.</returns>
     Task<byte[]> GetQRCode(string text);
 }
