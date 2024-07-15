6c39f998c1cc862364f746353a6318e6199da260
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-15 18:54:38 -0300

    Add DateHelper

---
+++ src/AthenasAcademy.Certificate.Core/Helpers/DateHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace AthenasAcademy.Certificate.Core.Helpers;
+
+    /// <summary>
+    /// The DateHelper class provides static helper methods for formatting date values used in the Athenas Academy application.
+    /// </summary>
+    public static class DateHelper
+    {
+        /// <summary>
+        /// Formats a DateTime value according to a specified format string.
+        /// </summary>
+        /// <param name="date">The DateTime value to be formatted.</param>
+        /// <param name="mask">The format string to use for formatting the date. Default is "dd MMMM yyyy".</param>
+        /// <returns>A string representing the formatted date.</returns>
+        /// <example>
+        /// Input: new DateTime(2024, 7, 15)
+        /// Output: "15 July 2024"
+        /// </example>
+        public static string FormatDate(DateTime date, string mask = "dd MMMM yyyy") 
+            => date.ToString(mask, CultureInfo.GetCultureInfo("en-US"));
+
+        /// <summary>
+        /// Formats a DateTime value with a location prefix and a specified format string.
+        /// </summary>
+        /// <param name="date">The DateTime value to be formatted.</param>
+        /// <param name="prefix">The location prefix to prepend to the formatted date. Default is "São Paulo - Brazil".</param>
+        /// <param name="mask">The format string to use for formatting the date. Default is "dd MMMM yyyy".</param>
+        /// <returns>A string representing the formatted date with the location prefix.</returns>
+        /// <example>
+        /// Input: new DateTime(2024, 7, 15)
+        /// Output: "São Paulo - Brazil, 15 July 2024"
+        /// </example>
+        public static string FormatLocationDate(DateTime date, string prefix = "São Paulo - Brazil", string mask = "dd MMMM yyyy") 
+            => $"{prefix}, {FormatDate(date, mask)}";
+    }
