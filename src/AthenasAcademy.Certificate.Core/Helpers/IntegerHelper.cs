ecb33acef517e53edd54ea54138eeb46aa59ea8e
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-15 18:55:42 -0300

    Add IntegerHelper

---
+++ src/AthenasAcademy.Certificate.Core/Helpers/IntegerHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace AthenasAcademy.Certificate.Core.Helpers;
+
+    /// <summary>
+    /// The IntegerHelper class provides static helper methods for formatting integer values used in the Athenas Academy application.
+    /// </summary>
+    public static class IntegerHelper
+    {
+        /// <summary>
+        /// Formats an integer workload value by appending a suffix.
+        /// </summary>
+        /// <param name="workload">The integer value representing the course workload.</param>
+        /// <param name="suffix">The suffix to append to the workload value. Default is "hours".</param>
+        /// <returns>A string representing the formatted workload value.</returns>
+        /// <example>
+        /// Input: 40
+        /// Output: "40 hours"
+        /// </example>
+        public static string FormatCourseWorkload(int workload, string suffix = "hours") 
+            => $"{workload} {suffix}";
+    }
